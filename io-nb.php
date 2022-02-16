a1e4408a735bc0222344caffa7f2170d9609e571
LcsCefali <lucas.cefali@gmail.com>
2022-02-16 10:39:03 -0300

    feat: created a socket server

--- io-nb.php
+++ io-nb.php
@@ -1,29 +1,39 @@
 <?php
 
 $streamList = [
+  stream_socket_client('tcp://localhost:8080'),
+  stream_socket_client('tcp://localhost:8001'),
   fopen('arquivo.txt', 'r'),
   fopen('arquivo2.txt', 'r')
 ];
 
+fwrite($streamList[0], 'GET /http-server.php HTTP/1.1'. PHP_EOL . PHP_EOL);
+
 foreach ($streamList as $stream) {
   stream_set_blocking($stream, false);
 }
-
-// arquivo esta pronto para leitura?
 
 do {
   $copyReadStream = $streamList;
   $streamsNumber = stream_select($copyReadStream, $write, $except, 0, 200000);
   
   if ($streamsNumber === 0) {
-    echo "realizar outras tarefas";
+    // echo "realizar outras tarefas". PHP_EOL;
     continue;
   }
 
   foreach ($copyReadStream as $key => $stream) {
-    echo fgets($stream);
+    $conteudo = stream_get_contents($stream);
+    $posicaoFimHttp = strpos($conteudo, PHP_EOL . PHP_EOL);
+
+    if ($posicaoFimHttp !== false) {
+      echo substr($conteudo, $posicaoFimHttp + 4);
+    } else {
+      echo $conteudo;
+    }
+    
     unset($streamList[$key]);
   }
 } while (!empty($streamList));
 
-echo "Todos os arquivos foram lidos!" . PHP_EOL;
+echo "Todos as streams foram lidas!" . PHP_EOL;
